2473fd95fbf36ec661616f2b21ff29af7dcc1f11
faxuexiaoxin <1300802930@qq.com>
2022-01-13 11:05:54 +0800

    第一个demo

--- demo1/src/test/java/top/faxuexiaoxin/dao/ExaminationDaoTest.java
+++ demo1/src/test/java/top/faxuexiaoxin/dao/ExaminationDaoTest.java
@@ -1,6 +1,5 @@
 package top.faxuexiaoxin.dao;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +15,28 @@ class ExaminationDaoTest {
     private ExaminationDao examinationDao;
     @Test
     void findDistinctByGrade() {
+        printAllStudent();
+
         final Examination examination = examinationDao.findDistinctByGrade(10);
-        Assertions.assertEquals("张三",examination);
+        Assertions.assertEquals("张三",examination.getName());
+    }
+
+    /**
+     * 打印表中所有的数据
+     */
+    private void printAllStudent() {
+        List<Examination> examinations = examinationDao.findAll();
+        System.out.println("表中所有的数据如下所示");
+        examinations.forEach(System.out::println);
+        System.out.println(" ======================================= ");
     }
 
     @Test
     void findAllByClassNo() {
+        printAllStudent();
         final List<Examination> examinations = examinationDao.findAllByClassNo("1402");
-        Assertions.assertEquals(2,examinations);
-        final String[] nameArrays = ((String[]) examinations.stream().map(Examination::getName).toArray());
+        Assertions.assertEquals(2,examinations.size());
+        final String[] nameArrays =  examinations.stream().map(Examination::getName).toArray(String[]::new);
         Assertions.assertArrayEquals(new String[]{"张三", "李四"},nameArrays);
     }
 }
---
+++ demo1/src/test/resources/schema.sql
@@ -0,0 +1,6 @@
+create table if not exists examination(
+    id int primary key auto_increment,
+    name varchar(10),
+    class_no varchar(10),
+    grade int
+);
